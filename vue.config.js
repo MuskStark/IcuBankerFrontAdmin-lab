bce210182c422049f2e32d3c4be0003aa37ad4a5
MuskStark <138420547+MuskStark@users.noreply.github.com>
2023-10-30 17:00:25 +0800

    optimize:优化打包结构

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.optimization.splitChunks({
+      chunks: 'all'
+    })
+  },
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {// 为生产环境修改配置...
+      config.mode = 'production';
+      config["performance"] = {//打包文件大小配置
+        "maxEntrypointSize": 10000000,
+        "maxAssetSize": 30000000
+      }
+    }
+  }
 })
